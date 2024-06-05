8d0648d8e667e2854156e05f23e2fbe119f3d63e
Subatomic87 <90572367+Subatomic87@users.noreply.github.com>
2024-06-05 17:18:24 -0400

    Add files via upload

---
+++ SPLITemail.py
@@ -0,0 +1,12 @@
+line="From samvith.muramula@gmail.com Sat Jan 5 09:14:16 2008"
+fhand = open("mbox-short.txt")
+for line in fhand:
+    line=line.rstrip
+    if not line.startswith("From"):continue
+line=line.rstrip
+words = line.split()
+print(words)
+email=words[1]
+pieces=email.split("@")
+name=pieces[0]
+print(name)
